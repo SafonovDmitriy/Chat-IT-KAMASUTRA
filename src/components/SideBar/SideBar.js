1bbfebad6e099a9a0f41df378ba4aa66a24d40a9
SafonovDmitriy <49404206+SafonovDmitriy@users.noreply.github.com>
2020-03-23 20:12:01 +0200

    day2

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -3,14 +3,14 @@ import React from 'react';
 
 import './SideBar.css'
 const SideBar = () => {
-    return <div className="app-wrapper">
-  <ul>
-    <li><a href = "#1">Profile</a></li>
-    <li><a href = "#2">Messages</a></li>
-    <li><a href = "#3">News</a></li>
-    <li><a href = "#4">Music</a></li>
-    <li><a href = "#5">Setting</a></li>
-  </ul>
-    </div>
-  };
-  export default SideBar
+  return <div className="SideBar-wrapper">
+    <ul>
+      <li><a href="#1">Profile</a></li>
+      <li><a href="#2">Messages</a></li>
+      <li><a href="#3">News</a></li>
+      <li><a href="#4">Music</a></li>
+      <li><a href="#5">Setting</a></li>
+    </ul>
+  </div>
+};
+export default SideBar

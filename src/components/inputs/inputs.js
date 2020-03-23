ffd7728e97f26674c6222e435e61b99c8a1f6f92
SafonovDmitriy <49404206+SafonovDmitriy@users.noreply.github.com>
2020-03-23 16:26:54 +0200

    day2
    day2

--- src/components/inputs/inputs.js
+++ src/components/inputs/inputs.js
@@ -3,10 +3,10 @@ import React from 'react';
 
 import './inputs.css'
 const TextBox = (value) => {
-    return <input className="TextBox" type= "text" value={value} />
-  };
-  const StyllButton = (value) => {
-    return <input className="StyllButton" type= "button" value={value}/>
-  };
+  return <input className="TextBox" type="text" value={value} />
+};
+const StyllButton = (value) => {
+  return <input className="StyllButton" type="button" value={value} />
+};
 
-  export {TextBox,StyllButton}
+export { TextBox, StyllButton }

dbac3d2e67990bec7fab6228743feeb21c04aed3
SafonovDmitriy <49404206+SafonovDmitriy@users.noreply.github.com>
2020-03-25 15:54:08 +0200

    day3

--- src/components/inputs/inputs.js
+++ src/components/inputs/inputs.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 
-import './inputs.css'
+import s from './inputs.module.css'
 const TextBox = (value) => {
-  return <input className="TextBox" type="text" placeholder={value} />
+  return <input className={s.TextBox} type="text" placeholder={value} />
 };
 const StyllButton = (value) => {
-  return <input className="StyllButton" type="button" placeholder={value} />
+  return <input className={s.StyllButton} type="button" placeholder={value} />
 };
 
 export { TextBox, StyllButton }

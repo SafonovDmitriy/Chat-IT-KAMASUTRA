8040fd754e9373c10ca97f5e1463a4afa5de8d19
SafonovDmitriy <49404206+SafonovDmitriy@users.noreply.github.com>
2020-03-27 00:49:06 +0200

    day4

--- src/App.js
+++ src/App.js
@@ -4,20 +4,26 @@ import Header from './components/Header/Header.jsx';
 import SideBar from './components/SideBar/SideBar.jsx';
 import Content from './components/Content/Content.jsx';
 import Dialogs from './components/Dialogs/Dialogs';
+import { Route, BrowserRouter } from 'react-router-dom';
 
 
 
 function App() {
   return (
+    <BrowserRouter>
+      <div className="App">
+        <div className="appHeader"><Header /></div>
+        <div className="appSideBar">  <SideBar /></div>
+        <div className="appContent">
+          <Route path="/profile" component={Content} />
+          <Route path="/messages" component={Dialogs} />
+          <Route path="/news"  />
+          <Route path="/music"  />
+          <Route path="/setting"  />
 
-    <div className="App">
-      <div className="appHeader"><Header /></div>
-      <div className="appSideBar">  <SideBar /></div>
-      <div className="appContent">
-        {/* <Content /> */}
-        <Dialogs />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 
 import s from './SideBar.module.css'
+import { NavLink } from 'react-router-dom';
 const SideBar = () => {
   return <div className={s.wrapper}>
     <ul>
-      <li><a className={s.active} href="#1">Profile</a></li>
-      <li><a href="#2">Messages</a></li>
-      <li><a href="#3">News</a></li>
-      <li><a href="#4">Music</a></li>
-      <li><a href="#5">Setting</a></li>
+      <li><NavLink to="profile" activeClassName={s.active}>Profile</NavLink></li>
+      <li><NavLink to="messages" activeClassName={s.active}>Messages</NavLink></li>
+      <li><NavLink to="news" activeClassName={s.active}>News</NavLink></li>
+      <li><NavLink to="music" activeClassName={s.active}>Music</NavLink></li>
+      <li><NavLink to="setting" activeClassName={s.active}>Setting</NavLink></li>
     </ul>
   </div>
 };
